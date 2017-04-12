d9ed188ec9df4f8966f26e04c93e44d79110c928
Juan Manuel Chaguendo <juanman90@gmail.com>
2017-04-12 09:23:44 -0500

    update footer and json resume

--- app/components/section/footer.js
+++ app/components/section/footer.js
@@ -20,11 +20,11 @@ const Footer = React.createClass({
                             <li>
                                 This site is developed in React.js by&nbsp;
                                 <a
-                                    href='https://suddi.github.io'
-                                    title='Sudharshan Ravindran'
+                                    href='https://juanmancb90.github.io/portafolio'
+                                    title='Juan Manuel'
                                     target='_blank'
                                     rel='noopener noreferrer'>
-                                    Sudharshan Ravindran
+                                   Juan Manuel
                                 </a> from the original design of Ceevee from&nbsp;
                                 <a
                                     href='http://www.styleshout.com/'
